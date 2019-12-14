0f334925d5187b5313bbf330bdd47b3bab1a752e
Maxim <maximb+bitbucket@inloop.com>
2019-12-14 17:13:19 +0200

    created service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-home',
@@ -7,15 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  h1Style: boolean = false;
-
-  constructor() { }
+  constructor(private data: DataService) { }
 
   ngOnInit() {
   }
 
   firstClick() {
-    this.h1Style = !this.h1Style;
+    this.data.firstClick();
   }
 
 }
