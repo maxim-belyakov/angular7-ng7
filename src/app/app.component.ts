915ceebbbb6f1d72714d5950fa6ffdb259d26629
Maxim <maximb+bitbucket@inloop.com>
2019-12-14 15:34:56 +0200

    components nav, home, about, contact

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'ng7';
+  title = 'angular7-ng7';
 }
